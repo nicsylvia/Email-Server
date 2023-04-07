9190f219ec7c137ac50615b4c039dee916c9820c
nicsylvia <nicsylvia15f@gmail.com>
2023-04-07 12:56:49 -0700

    more updates on email sever

--- src/Utils/Email.ts
+++ src/Utils/Email.ts
@@ -12,3 +12,27 @@ const GOOGLE_REDIRECT: string = "";
 
 // oAUTH is open authentication, we want to tap to google's authentication
 const oAUTH = new google.auth.oAUTH(GOOGLE_ID, GOOGLE_SECRET, GOOGLE_REDIRECT);
+
+export const VerifyAccount = async () => {
+  try {
+    oAUTH.setCredentials({ access_token: GOOGLE_REFRESHTOKEN });
+
+    const GetToken = await oAUTH.getAccessToken;
+
+    // Nodemailer aspect:
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        clientId: GOOGLE_ID,
+        clientSecret: GOOGLE_SECRET,
+        refreshToken: GOOGLE_REFRESHTOKEN,
+        accessToken: GetToken?.token,
+      },
+    });
+
+    const MailerOption = {};
+  } catch (error) {
+    console.log(error);
+  }
+};
